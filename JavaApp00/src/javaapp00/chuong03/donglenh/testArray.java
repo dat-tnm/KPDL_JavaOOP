c5590a0679484cbce34acfffe1f6dd7c761457e1
Admin <tnmdat183@gmail.com>
2021-03-01 10:49:28 +0700

    bai13 - 01 - mang 1 chieu

---
+++ JavaApp00/src/javaapp00/chuong03/donglenh/testArray.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp00.chuong03.donglenh;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class testArray {
+    public static void main(String[] args){
+//        int[] a = new int[4];
+//        a[0] = 12;
+//        a[1] = 9;
+//        a[2] = 1;
+//        a[3] = 15;
+//        
+//        System.out.println(Arrays.toString(a));
+//        
+//        int[] b = new int[]{1, 2 , -7, 8, 9 , 20};
+//        System.out.println(Arrays.toString(b));
+//        System.out.println("So phan tu cua mang b: "+b.length);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so phan tu cua mang: ");
+        int n = Integer.parseInt(sc.nextLine());
+        int[] c = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            System.out.print("c[" + i + "] = ");
+//            c[i] = Integer.parseInt(sc.nextLine());
+//        }
+//        System.out.println("Gia tri cua mang vua nhap la:");
+//        System.out.println(Arrays.toString(c));
+//        for (int i = 0; i < c.length; i++) {
+//            System.out.print(c[i] + " ");
+//        }
+        
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++) {
+            c[i] = rnd.nextInt(1000);
+        }
+        System.out.println(Arrays.toString(c));
+    }
+}
